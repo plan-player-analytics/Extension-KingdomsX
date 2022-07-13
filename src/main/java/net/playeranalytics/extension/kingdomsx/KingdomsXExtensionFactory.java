d5dd5e1d61be77d9bc947332669699b26000a3a2
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2022-07-13 10:03:22 +0300

    R1.4: Package change, junit scope

--- src/main/java/net/playeranalytics/extension/kingdomsx/KingdomsXExtensionFactory.java
+++ src/main/java/net/playeranalytics/extension/kingdomsx/KingdomsXExtensionFactory.java
@@ -20,7 +20,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
     THE SOFTWARE.
 */
-package com.djrapitops.extension;
+package net.playeranalytics.extension.kingdomsx;
 
 import com.djrapitops.plan.extension.DataExtension;
 
